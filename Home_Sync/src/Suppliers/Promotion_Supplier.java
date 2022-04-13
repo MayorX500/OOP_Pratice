631dd343ccdc4ef5f9f4e15618be72cef638b38e
MayorX500 <angola2001@gmail.com>
2022-04-13 19:22:18 +0100

    Added Suppliers and Promotional_Supplier

---
+++ Home_Sync/src/Suppliers/Promotion_Supplier.java
@@ -0,0 +1,60 @@
+package Suppliers;
+
+public class Promotion_Supplier extends Suppliers{
+	private float promotion_price;
+
+
+	public Promotion_Supplier(int supplier_id, String supplier_name, float base_price, float tax, float out_of_range_tax,float promotion_price) {
+		super(supplier_id,supplier_name,base_price,tax,out_of_range_tax);
+		this.promotion_price = promotion_price;
+	}
+
+	public Promotion_Supplier(String supplier_name, float base_price, float tax, float out_of_range_tax,float promotion_price) {
+		super(supplier_name,base_price,tax,out_of_range_tax);
+		this.promotion_price = promotion_price;
+	}
+
+	public Promotion_Supplier() {
+		this("Promotion",1f,0.13f,0.05f,0.75f);
+	}
+
+	public Promotion_Supplier(Promotion_Supplier o){
+		this(o.getSupplier_id(),o.getSupplier_name(),o.getBase_price(),o.getTax(),o.getOut_of_range_tax(),o.getPromotion_price());
+	}
+
+	public float getPromotion_price() {
+		return this.promotion_price;
+	}
+
+	public void setPromotion_price(float promotion_price) {
+		this.promotion_price = promotion_price;
+	}
+
+	public Promotion_Supplier promotion_price(float promotion_price) {
+		setPromotion_price(promotion_price);
+		return this;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == this)
+			return true;
+		if (!(o instanceof Promotion_Supplier)) {
+			return false;
+		}
+		Promotion_Supplier promotion_Supplier = (Promotion_Supplier) o;
+		return promotion_price == promotion_Supplier.promotion_price;
+	}
+
+	@Override
+	public String toString() {
+		return "{" +
+			" promotion_price='" + getPromotion_price() + "'" +
+			"}";
+	}
+
+	@Override
+	public Promotion_Supplier clone(){
+		return new Promotion_Supplier(this);
+	}
+}
---
+++ Home_Sync/src/Suppliers/Suppliers.java
@@ -0,0 +1,77 @@
+package Suppliers;
+
+import java.util.concurrent.atomic.*;
+
+public class Suppliers {
+	private static final AtomicInteger count = new AtomicInteger(0);
+	private int supplier_id;
+	private String supplier_name;
+	private float base_price;
+	private float tax;
+	private float out_of_range_tax;
+
+	public Suppliers(int supplier_id, String supplier_name, float base_price, float tax, float out_of_range_tax) {
+		this.supplier_id = supplier_id;
+		this.supplier_name = supplier_name;
+		this.base_price = base_price;
+		this.tax = tax;
+		this.out_of_range_tax = out_of_range_tax;
+	}
+
+	public Suppliers(String supplier_name, float base_price, float tax, float out_of_range_tax) {
+		this.supplier_id = count.incrementAndGet();
+		this.supplier_name = supplier_name;
+		this.base_price = base_price;
+		this.tax = tax;
+		this.out_of_range_tax = out_of_range_tax;
+	}
+
+	public Suppliers(){
+		this("Supplier", 1.0f, 0.13f, 0.05f);
+	}
+
+	public Suppliers(Suppliers o){
+		this(o.getSupplier_id(),o.getSupplier_name(),o.getBase_price(),o.getTax(),o.getOut_of_range_tax());
+	}
+
+	public int getSupplier_id() {
+		return this.supplier_id;
+	}
+
+	public void setSupplier_id(int supplier_id) {
+		this.supplier_id = supplier_id;
+	}
+
+	public String getSupplier_name() {
+		return this.supplier_name;
+	}
+
+	public void setSupplier_name(String supplier_name) {
+		this.supplier_name = supplier_name;
+	}
+
+	public float getBase_price() {
+		return this.base_price;
+	}
+
+	public void setBase_price(float base_price) {
+		this.base_price = base_price;
+	}
+
+	public float getTax() {
+		return this.tax;
+	}
+
+	public void setTax(float tax) {
+		this.tax = tax;
+	}
+
+	public float getOut_of_range_tax() {
+		return this.out_of_range_tax;
+	}
+
+	public void setOut_of_range_tax(float out_of_range_tax) {
+		this.out_of_range_tax = out_of_range_tax;
+	}
+
+}
