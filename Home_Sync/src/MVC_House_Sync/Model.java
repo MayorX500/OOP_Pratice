9aee3fb16e234f5781b5d3f2eb8b274e85cf2e89
Gon96923 <g.vale2002@gmail.com>
2022-04-15 18:53:27 +0100

    Changes to medel

---
+++ Home_Sync/src/MVC_House_Sync/Model.java
@@ -0,0 +1,43 @@
+package MVC_House_Sync;
+import House.*;
+import SmartDevice.*;
+import Suppliers.*;
+
+public class Model{
+    public void newDate(){}//avançar tempo
+
+    public void turnDivision(House home, String divis, boolean state){
+
+        //home.divisionOnOff(divis,state);
+
+    }//turn on/off entire division
+
+    public void turnDevice(House home, String divis, SmartDevice device, boolean state){
+        //home.turnOneOnOFF(divis,device,state);
+    }//turn on/off specific device
+
+    public void swapProvider(House home, Suppliers f){
+        home.setSupplier(f);
+    }//swap energy provider
+
+    public void alterProviderbase_price(Suppliers f,float value){
+        f.setBase_price(value);
+    }//alter values of energy provider
+
+    public void alterProvidertax(Suppliers f,float value){
+        f.setTax(value);
+    }//alter values of energy provider
+
+    public void alterProviderout_of_range_tax(Suppliers f,float value){
+        f.setOut_of_range_tax(value);
+    }//alter values of energy provider
+
+    public void statistics(){
+
+    }//calculate and show statistics
+
+    public void saveFile(){
+
+    }//ponto 5 do enunciado; salvar o estado da aplicação a qualquer momento
+
+}
